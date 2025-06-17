a10b23aba38f7ba589b54e578ea04360aa66b735
portailferforge <portails.ferforge@gmail.com>
2025-06-17 12:32:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function lancerCopilotte() {
   const status = document.getElementById("status");
   status.textContent = "Copilotte lancée... Système prêt ✅";
+  document.body.classList.add("loaded");
 }
